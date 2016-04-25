0f849c4bf6b0f3d5259aee5cb122ef187017b2a7
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2016-04-25 21:55:28 +0300

    Create SelectionSort.cs

---
+++ hw_1/src/SelectionSort.cs
@@ -0,0 +1,17 @@
+static void SelectionSort(int[] mas)
+{
+            for (int i = 0; i < (mas.Length - 1); i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = mas[i];
+                mas[i] = mas[min];
+                mas[min] = temp;
+            }
+        }
