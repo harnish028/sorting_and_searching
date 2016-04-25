d58afdc79574a824fb46a2920fc1dfc42e1446ac
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2016-04-25 22:01:19 +0300

    Update BubbleSort.cs

--- hw_1/src/BubbleSort.cs
+++ hw_1/src/BubbleSort.cs
@@ -1,1 +1,16 @@
-
+static void BubbleSort(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        for (int j = 0; j < mas.Length - i - 1; j++)
+        {
+            if (mas[j] < mas[j + 1])
+            {
+                int temp = mas[j];
+                mas[j] = mas[j + 1];
+                mas[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
