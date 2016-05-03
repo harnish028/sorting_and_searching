82d85d94623dd15323821477a6615a59c8d0649b
Dmitry Parshikov <dmitry.parshikov28@gmail.com>
2016-05-04 02:33:53 +0300

    Update MultiwayMergeSort.cs

--- hw_2/src/MultiwayMergeSort.cs
+++ hw_2/src/MultiwayMergeSort.cs
@@ -5,42 +5,55 @@ public static class MultiwayMergeS
         if(left < right)
         {
             int lenght = (right - left) / parts + 1;
-            Merge(arr, left, right, lenght, parts);
+            parts = (right - left + 1) / length + 1;
+            int lenghtOfLastArr = right - (left + (parts - 1) * lenght) + 1;
+            
+            for (int i = 0; i < parts - 1; i++)
+                MultiwayMergeSort(arr,  left + lenght * i, left + lenght * (i + 1) - 1, parts);
+            MultiwayMergeSort(arr, left + lenght * (parts - 1), left, parts);
+            Merge(arr, left, right, lenght, lenghtOfLastArr, parts);
         }
     }
 
-    public static void Merge(int[] arr, int left, int right, int lenght, int parts)
+    public static void Merge(int[] arr, int left, int right, int lenght, lenghtOfLastArr, int parts)
     {
-        int[][] array = new int[parts][];
+        int[][] tempArray = new int[parts][];
 
-        for (int i = 0; i < parts; i++)
+        for (int i = 0; i < parts - 1; i++)
         {
-            array[i] = new int[lenght + 1];
+            tempArray[i] = new int[lenght + 1];
         }
+        tempArray[parts - 1] = new int[lenghtOfLastArr + 1];
 
-        for(int i = 0;  i < parts; i++)
+        for (int i = 0; i < parts - 1; i++)
         {
-            for(int j = 0; j < lenght; j++)
+            for (int j = 0; j < lenght; j++)
             {
-                array[i][j] = arr[left + lenght * i + j];
+                tempArray[i][j] = arr[left + lenght * i + j];
             }
-            array[i][lenght + 1] = int.MaxValue;
+            tempArray[i][lenght] = int.MaxValue;
         }
+
+        for (int i = 0; i < lenghtOfLastArr; i++)
+        {
+            tempArray[parts - 1][i] = arr[left + ((parts - 1) * lenght) + 1];
+        }
+        tempArray[parts - 1][lenghtOfLastArr] = int.MaxValue;
 
         int[] indexes = new int[parts];
         int min = 0;
 
-        for(int i = left; i < right; i++)
+        for (int i = left; i <= right; i++)
         {
-            for(int j = 0; j < parts; j++)
+            for (int j = 0; j < parts; j++)
             {
-                if(array[j][indexes[j]] < array[min][indexes[min]])
+                if (tempArray[j][indexes[j]] < tempArray[min][indexes[min]])
                 {
                     min = j;
                 }
-                arr[i] = array[min][indexes[min]];
-                indexes[min]++;
             }
+        arr[i] = tempArray[min][indexes[min]];
+        indexes[min]++;    
         }
     }
 }
